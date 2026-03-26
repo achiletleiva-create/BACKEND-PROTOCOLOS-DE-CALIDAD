55edf4e136ceeaaef58225635816818cc8159e39
achiletleiva-create <achiletleiva@gmail.com>
2026-03-26 01:39:30 -0500

    Add files via upload

--- config/db.js
+++ config/db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 
 mongoose
@@ -7,13 +6,3 @@ mongoose
   .catch((err) => console.error('❌ Error de conexión:', err));
 
 module.exports = mongoose;
-=======
-const mongoose = require('mongoose');
-
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => console.log('✅ Conectado a la BD de Ingeniería'))
-  .catch((err) => console.error('❌ Error de conexión:', err));
-
-module.exports = mongoose;
->>>>>>> f797d30 (Add all project files: app, config, middleware, models, routes, services, utils + .gitignore)
